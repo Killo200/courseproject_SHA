52a0a0085d230c269e52513bf731807dc154b5da
Killo200 <gauss-nsk@yandex.ru>
2021-02-01 19:46:17 +0300

    create project on spring, hibernate, aspect

---
+++ src/main/java/ru/killo200/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package ru.killo200.spring.mvc_hibernate_aop.dao;
+
+import ru.killo200.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
---
+++ src/main/java/ru/killo200/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package ru.killo200.spring.mvc_hibernate_aop.service;
+
+import ru.killo200.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
